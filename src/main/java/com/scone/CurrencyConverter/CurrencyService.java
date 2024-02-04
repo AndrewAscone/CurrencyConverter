d2940a7f276d9014198dfb5b83ff29f83984ae13
AndrewAscone <andrew.ascone@gmail.com>
2024-02-04 13:28:01 -0500

    Beginning work on service class

--- src/main/java/com/scone/CurrencyConverter/CurrencyService.java
+++ src/main/java/com/scone/CurrencyConverter/CurrencyService.java
@@ -1,4 +1,20 @@
 package com.scone.CurrencyConverter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class CurrencyService {
+
+    @Value("${exchange.api.url}")
+    private String apiUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public double convertCurrency(String sourceCurrency, String targetCurrency, double amount){
+        return 0;
+    }
 }
