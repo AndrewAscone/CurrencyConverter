3758cd1984343ab29b8ace2c9b8aafc144ea14ca
AndrewAscone <andrew.ascone@gmail.com>
2024-02-04 13:04:40 -0500

    Started filling out controller class

--- src/main/java/com/scone/CurrencyConverter/CurrencyController.java
+++ src/main/java/com/scone/CurrencyConverter/CurrencyController.java
@@ -1,4 +1,26 @@
 package com.scone.CurrencyConverter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/currency")
 public class CurrencyController {
+
+    @Autowired
+    private CurrencyService currencyService;
+
+    @GetMapping("/convert")
+    public ResponseEntity<Double> convertCurrency(
+        @RequestParam String sourceCurrency,
+        @RequestParam String targetCurrency,
+        @RequestParam double amount) {
+
+        double convertedAmount = currencyService.convertCurrency(sourceCurrency, targetCurrency, amount);
+        return ResponseEntity.ok(convertedAmount);
+    }
 }
