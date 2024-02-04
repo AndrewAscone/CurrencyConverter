317b62571e5e9717df7859f1275d93c95d8403c5
AndrewAscone <andrew.ascone@gmail.com>
2024-02-04 13:28:44 -0500

    Adding some comments

--- src/main/java/com/scone/CurrencyConverter/CurrencyService.java
+++ src/main/java/com/scone/CurrencyConverter/CurrencyService.java
@@ -15,6 +15,9 @@ public class CurrencyService {
     private RestTemplate restTemplate;
 
     public double convertCurrency(String sourceCurrency, String targetCurrency, double amount){
+        // Use restTemplate to make HTTP requests to the currency exchange rate API
+        // Implement the logic to parse the API response and perform the currency conversion
+        // Return the converted amount
         return 0;
     }
 }
