995c0976ea763e275fde91fcfd98b7965ed80884
AndrewAscone <andrew.ascone@gmail.com>
2024-02-26 16:05:57 -0500

    Created CurrencyApiResponse DTO, starting to fill out conversion method in service class

--- src/main/java/com/scone/CurrencyConverter/CurrencyService.java
+++ src/main/java/com/scone/CurrencyConverter/CurrencyService.java
@@ -2,22 +2,33 @@ package com.scone.CurrencyConverter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CurrencyService {
-
+    //@Value annotation is injecting the URL from application.properties to variable
+    //Placeholder for base URL of API endpoint
     @Value("${exchange.api.url}")
     private String apiUrl;
 
+    //This is a Spring class used to simplify making HTTP requests, injected into scope via @Autowired
+    //This is used to send an HTTP GET request to currency exchange rate API
     @Autowired
     private RestTemplate restTemplate;
 
     public double convertCurrency(String sourceCurrency, String targetCurrency, double amount){
-        // Use restTemplate to make HTTP requests to the currency exchange rate API
-        // Implement the logic to parse the API response and perform the currency conversion
-        // Return the converted amount
-        return 0;
+        String url = apiUrl + "?base=" + sourceCurrency + "&symbols=" + targetCurrency;
+
+        ResponseEntity<CurrencyApiResponse> responseEntity = restTemplate.getForEntity(url, CurrencyApiResponse.class);
+        CurrencyApiResponse response = responseEntity.getBody();
+
+        if (response != null && response.getRates() != null && response.getRates().containsKey(targetCurrency)) {
+            double exchangeRate = response.getRates().get(targetCurrency);
+            return amount * exchangeRate;
+        } else {
+            throw new RuntimeException("Failed to fetch exchange rate or invalid currency code");
+        }
     }
 }
